988c5b0d7dbfaebd13657922a3fda4d86a263d5b
gustavomotta-twrt <gustavo.s@twrt.com.br>
2025-04-02 13:58:36 -0300

    fix errors from vercel

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,17 +1,5 @@
 "use client";
 
-import Image from "next/image";
-import LinearProgress, {
-  linearProgressClasses,
-} from "@mui/material/LinearProgress";
-import dayjs, { Dayjs } from "dayjs";
-import { useState } from "react";
-import { Button, Rating, styled } from "@mui/material";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
-import { EmblaOptionsType } from "embla-carousel";
-import EmblaCarousel from "@/components/Carousel/Carousel";
 export default function HomePage() {
   return (
     <h1 className="text-red-600 text-xl">
