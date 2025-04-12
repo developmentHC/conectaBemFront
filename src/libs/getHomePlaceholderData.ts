398bed9e06140addd6f8b78dbda092551e031745
gustavomotta-twrt <gustavomottadeveloper@gmail.com>
2025-04-12 16:48:09 -0300

    Revert "fix build errors"
    This reverts commit 3e987670d946ead4336ad647749bd7a1f78c0f72.

--- src/libs/getHomePlaceholderData.ts
+++ src/libs/getHomePlaceholderData.ts
@@ -11,34 +11,30 @@ export async function getHomePlaceholderData() {
             throw new Error("Nenhum dado encontrado para 'home'.");
             }
 
-            const { h1_text, description, banner, especialidades_em_destaque } = menuResponse[0].data;
+      const { h1_text, description, banner, especialidades_em_destaque } =
+        menuResponse[0].data;
 
-            // IDs das especialidades para buscar os nomes completos
-            const especialidadesIds = especialidades_em_destaque.map(
-            (item) => item.lista_de_especialidades.id
-            );
+      const especialidadesIds = especialidades_em_destaque.map(
+        (item) => item.lista_de_especialidades.id
+      );
 
-            // Buscar os documentos das especialidades
-            const especialidadesDocs = await client.getByIDs(especialidadesIds, { lang: "pt-br" });
+      const especialidadesDocs = await client.getByIDs(especialidadesIds, {
+        lang: "pt-br",
+      });
 
-            console.log("especialidadesDocs", especialidadesDocs);
+      const especialidades = especialidadesDocs.results.map((doc) => ({
+        id: doc.id,
+        nome: doc.data?.name || doc.slugs[0] || "Nome não disponível",
+      }));
 
-            // Mapeia os documentos para extrair nome e ID
-            const especialidades = especialidadesDocs.results.map((doc) => ({
-                id: doc.id,
-                nome: doc.data?.name || doc.slugs[0] || "Nome não disponível",
-            }));
-
-            console.log({
-            titulo: h1_text,
-            descricao: description,
-            bannerUrl: banner?.url || "",
-            especialidades,
-            });
-
-        } catch (error) {
-            console.error("Erro ao buscar dados do Prismic:", error);
-        }
+      console.log({
+        titulo: h1_text,
+        descricao: description,
+        bannerUrl: banner?.url || "",
+        especialidades,
+      });
+    } catch (error) {
+      console.error("Erro ao buscar dados do Prismic:", error);
     }
 
     return await fetchHomeData();   
