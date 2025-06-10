c6dd09cf1b1aea9c58fd1e1fdbedac454393738e
nathalia-84 <nathaliaazmelo@gmail.com>
2025-06-10 15:08:55 -0300

    fix code input on error border color

--- src/components/CodeInput/types.ts
+++ src/components/CodeInput/types.ts
@@ -2,4 +2,5 @@ export type CodeInputProps = {
   value: (string | null)[];
   onChange?: (value: (string | null)[]) => void;
   onFirstComplete?: (value: (string | null)[]) => void;
+  error?: boolean | null;
 };
