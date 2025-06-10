c5c86d9bf54cfb0a7b689d8751c96c30ab25098f
nathalia-84 <nathaliaazmelo@gmail.com>
2025-06-10 15:14:48 -0300

    fix code input on error border color

--- src/components/CodeInput/index.tsx
+++ src/components/CodeInput/index.tsx
@@ -71,7 +71,7 @@ export const CodeInput = forwardRef<CodeInputHandle, CodeInputProps>(({ value, o
           key={index}
           className={`px-2 py-6 rounded-lg w-full text-center border-2 ${
             error
-              ? "border-red-600 focus:border-red-600"
+              ? "border-red-600 focus:outline-red-400"
               : "border-input-code-border focus:outline-blue-600"
           }  text-inputCodeText font-bold transition-all`}
           type="number"
