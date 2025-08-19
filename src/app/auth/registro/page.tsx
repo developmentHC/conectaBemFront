3bf7771a01f2b70a9205fbadc5e6ef34f896376b
JulioVieiraP <134452045+JulioVieiraP@users.noreply.github.com>
2025-08-19 13:20:00 -0300

    feat: removendo botão de voltar e adicionando nova estilização para os buttons do registro

--- src/app/auth/registro/page.tsx
+++ src/app/auth/registro/page.tsx
@@ -35,15 +35,15 @@ export default function Registro() {
             <FormMultiStep.BackStepButton />
           </Link> */}
           <FormMultiStep.Title>Tudo pronto para começar</FormMultiStep.Title>
-          <FormMultiStep.Progress progress={20} />
+          {/* <FormMultiStep.Progress progress={20} /> */}
           <FormMultiStep.Description>Você deseja se cadastrar como:</FormMultiStep.Description>
         </FormMultiStep.Header>
-        <div className="flex flex-col gap-6">
-          <Button className="text-black w-full" variant="outlined" onClick={handleOpen}>
+        <div className="flex flex-col gap-5">
+          <Button className="w-full" sx={{ borderRadius: "4px", color: "#3857F4", borderColor: "#3857F4", padding: "12px 0 12px" }} variant="outlined" onClick={handleOpen}>
             Paciente
           </Button>
           <Link href={"/auth/registro-profissional"}>
-            <Button className="text-black w-full" variant="outlined">
+            <Button className="w-full" sx={{ borderRadius: "4px", color: "#3857F4", borderColor: "#3857F4", padding: "12px 0 12px" }} variant="outlined">
               Profissional
             </Button>
           </Link>
@@ -84,14 +84,18 @@ export default function Registro() {
           </Typography>
           <Box sx={{ display: "flex", flexDirection: "column", gap: "12px", width: "100%", mt: "8px" }}>
             <Link href="/auth/registro-paciente" style={{ width: "100%" }}>
-              <Button variant="contained" color="primary" size="large" fullWidth>
+              <Button variant="contained" color="primary" size="large" fullWidth sx={{ padding: "12px 0" }}>
                 Prosseguir Com Cadastro
               </Button>
             </Link>
-            <Button variant="outlined" size="large" onClick={handleClose} fullWidth>
-              Voltar
+            <Button variant="outlined" size="large" onClick={handleClose} fullWidth sx={{ padding: "12px 0" }}>
+              Cancelar
             </Button>
           </Box>
+          <Typography variant="body2" sx={{ mt: "24px", gap: "4px", color: "#645D6F" }} className="flex break-words">
+            <InfoIcon className="fill-input-code-border" height={20} width={21} />
+            Saiba mais sobre os perfis disponíveis no ConectaBem
+          </Typography>
         </Box>
       </Modal>
     </main>
