d509277ed8b49e3b097c57d20a96f99b8d5ffd33
PolyannaMeira <polyannaxam@gmail.com>
2025-10-02 17:23:18 +0200

    feat: add accessibility step for professional and update patient step with skip option

--- src/features/auth/components/ProfissionalRegister/FormTitle.tsx
+++ src/features/auth/components/ProfissionalRegister/FormTitle.tsx
@@ -9,20 +9,26 @@ export const FormTitle = () => {
       {step === "personal_data" && (
         <>
           <FormMultiStep.Title>Cadastro profissional</FormMultiStep.Title>
-          <span className="text-gray-500 text-sm">etapa 1/4</span>
+          <span className="text-gray-500 text-sm">etapa 1/5</span>
         </>
       )}
       {step === "service_location" && (
         <>
           <FormMultiStep.Title>Cadastro local de atendimento</FormMultiStep.Title>
 
-          <span className="text-gray-500 text-sm">etapa 2/4</span>
+          <span className="text-gray-500 text-sm">etapa 2/5</span>
         </>
       )}
       {step === "specialties" && (
         <>
           <FormMultiStep.Title>Especialidades</FormMultiStep.Title>
-          <span className="text-gray-500 text-sm">etapa 3/4</span>
+          <span className="text-gray-500 text-sm">etapa 3/5</span>
+        </>
+      )}
+      {step === "accessibility" && (
+        <>
+          <FormMultiStep.Title>Acessibilidade</FormMultiStep.Title>
+          <span className="text-gray-500 text-sm">etapa 4/5</span>
         </>
       )}
       {step === "complete_profile" && (
@@ -30,7 +36,7 @@ export const FormTitle = () => {
           <FormMultiStep.Title>
             Lembre de completar seu perfil
           </FormMultiStep.Title>
-          <span className="text-gray-500 text-sm">etapa 4/4</span>
+          <span className="text-gray-500 text-sm">etapa 5/5</span>
         </>
       )}
     </>
