8a85a66ac144904c8e2b037339cf3b3dcaef8d28
JulioVieiraP <134452045+JulioVieiraP@users.noreply.github.com>
2025-10-12 16:34:49 -0300

    fix: ajustar validações e mensagens de erro no formulário de registro

--- src/features/auth/components/ProfissionalRegister/PersonalDataStep.tsx
+++ src/features/auth/components/ProfissionalRegister/PersonalDataStep.tsx
@@ -9,21 +9,26 @@ import { DatePicker } from "@mui/x-date-pickers";
 import dayjs from "dayjs";
 import { useProfissionalRegisterStore } from "./useProfissionalRegisterStore";
 import { CEPField } from "@/components/Fields/CEPField";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useCEP } from "../../hooks/useCEP";
 
 const schema = z.object({
   name: z
     .string()
-    .min(10, "Nome inválido")
+    .min(3, "Nome inválido")
     .regex(
       /^[A-Za-zÀ-ú]+(?: [A-Za-zÀ-ú]+)*$/,
       "O nome deve conter apenas letras e um espaço entre as palavras"
     ),
   birthdate: z
-    .instanceof(Date)
+    .instanceof(Date, { message: "Data de nascimento inválida." })
+    .nullable()
+    .refine((date) => date !== null && !isNaN(date.getTime()), {
+        message: "Data de nascimento é obrigatória!",
+    })
     .refine(
       (date) => {
+        if (!date) return true; 
         const min = dayjs().subtract(110, "years").toDate();
         return date >= min;
       },
@@ -33,6 +38,7 @@ const schema = z.object({
     )
     .refine(
       (date) => {
+        if (!date) return true;
         const max = dayjs().subtract(18, "years").toDate();
         return date <= max;
       },
@@ -64,16 +70,16 @@ const schema = z.object({
       }
     ),
   enderecoResidencial: z.string().min(3, "Endereço inválido"),
+  numeroResidencial: z.string().min(1, "Número obrigatório"), 
   bairroResidencial: z.string().min(3, "Bairro inválido"),
   cidadeResidencial: z.string().min(3, "Cidade inválida"),
-  estadoResidencial: z.string().min(3, "Estado inválido"),
+  estadoResidencial: z.string().min(2, "Estado inválido"),
 });
 
 type Data = z.infer<typeof schema>;
 
 export const PersonalDataStep = () => {
   const { changeStep, updateFields } = useProfissionalRegisterStore();
-  const [nameInput, setNameInput] = useState("");
 
   const {
     register,
@@ -84,9 +90,25 @@ export const PersonalDataStep = () => {
   } = useForm<Data>({
     mode: "onSubmit",
     resolver: zodResolver(schema),
+    defaultValues: {
+      name: "",
+      birthdate: null,
+      cepResidencial: "",
+      enderecoResidencial: "",
+      numeroResidencial: "",
+      bairroResidencial: "",
+      cidadeResidencial: "",
+      estadoResidencial: "",
+    }
   });
 
+  const nameValue = watch("name"); 
   const cepValue = watch("cepResidencial");
+  const logradouroValue = watch("enderecoResidencial");
+  const bairroValue = watch("bairroResidencial");
+  const cidadeValue = watch("cidadeResidencial");
+  const estadoValue = watch("estadoResidencial");
+  const numeroResidencialValue = watch("numeroResidencial");
   const shouldFetchCep = cepValue?.replace(/\D/g, "").length === 8;
 
   const { data } = useCEP({
@@ -94,9 +116,14 @@ export const PersonalDataStep = () => {
   });
 
   const onSubmit = handleSubmit(async (data: Data) => {
-    data.cepResidencial = data.cepResidencial.replace("-", "");
+    const submittedData = {
+        ...data,
+        birthdate: data.birthdate === null ? undefined : data.birthdate,
+        cepResidencial: data.cepResidencial.replace("-", ""),
+        numeroResidencial: data.numeroResidencial ? parseInt(data.numeroResidencial, 10) : null,
+    }
 
-    updateFields(data);
+    updateFields(submittedData);
 
     window.scrollTo({ top: 0, behavior: "smooth" });
 
@@ -111,8 +138,7 @@ export const PersonalDataStep = () => {
       .replace(/\s+/g, " ")
       .trimStart();
 
-    setValue("name", onlyLettersAndSpace);
-    setNameInput(onlyLettersAndSpace);
+    setValue("name", onlyLettersAndSpace, { shouldValidate: true });
   };
 
   useEffect(() => {
@@ -122,28 +148,29 @@ export const PersonalDataStep = () => {
     setValue("bairroResidencial", data.bairro);
     setValue("cidadeResidencial", data.localidade);
     setValue("estadoResidencial", data.estado);
+    setValue("numeroResidencial", "");
   }, [data, setValue]);
 
   return (
-    <form onSubmit={onSubmit} className="flex flex-col gap-4">
+    <form onSubmit={onSubmit} className="flex flex-col gap-6">
       <div className="flex flex-col gap-2">
-        <label>
+        <label className={errors.name ? "text-red-600" : ""}>
           Nome Completo <span className="text-red-600">*</span>
         </label>
         <TextField
+          {...register("name")}
           onChange={replaceName}
-          value={nameInput}
+          value={nameValue}
           id="name"
           variant="outlined"
           placeholder="Nome e Sobrenome"
-          required
           helperText={errors.name?.message}
           error={!!errors.name}
         />
       </div>
 
       <div className="flex flex-col gap-2">
-        <label>
+        <label className={errors.birthdate ? "text-red-600" : ""}>
           Data de Nascimento <span className="text-red-600">*</span>
         </label>
         <DatePicker
@@ -157,7 +184,6 @@ export const PersonalDataStep = () => {
               },
               helperText: errors.birthdate?.message,
               error: !!errors.birthdate,
-              required: true,
             },
           }}
           onChange={(date) =>
@@ -169,7 +195,7 @@ export const PersonalDataStep = () => {
       </div>
 
       <div className="flex flex-col gap-2">
-        <label>
+        <label className={errors.cepResidencial ? "text-red-600" : ""}>
           CEP Residencial <span className="text-red-600">*</span>
         </label>
         <CEPField
@@ -180,6 +206,88 @@ export const PersonalDataStep = () => {
           error={!!errors.cepResidencial}
           helperText={errors.cepResidencial?.message}
         />
+        <div className="flex justify-end">
+          <span className="text-base text-[#1D1B20] underline cursor-pointer font-normal">
+            Buscar CEP
+          </span>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label className={errors.enderecoResidencial ? "text-red-600" : ""}>
+          Longradouro <span className="text-red-600">*</span>
+        </label>
+        <TextField
+          {...register("enderecoResidencial")}
+          value={logradouroValue}
+          variant="outlined"
+          placeholder="Nome da rua / avenida, número"
+          helperText={errors.enderecoResidencial?.message}
+          error={!!errors.enderecoResidencial}
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label className={errors.numeroResidencial ? "text-red-600" : ""}>
+          Número <span className="text-red-600">*</span>
+        </label>
+        <TextField
+          {...register("numeroResidencial")}
+          value={numeroResidencialValue}
+          type="number"
+          variant="outlined"
+          placeholder="0000"
+          helperText={errors.numeroResidencial?.message}
+          error={!!errors.numeroResidencial}
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label className={errors.bairroResidencial ? "text-red-600" : ""}>
+          Bairro <span className="text-red-600">*</span>
+        </label>
+        <TextField
+          {...register("bairroResidencial")}
+          value={bairroValue}
+          variant="outlined"
+          placeholder="Nome do bairro"
+          helperText={errors.bairroResidencial?.message}
+          error={!!errors.bairroResidencial}
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label className={errors.cidadeResidencial ? "text-red-600" : ""}>
+          Cidade <span className="text-red-600">*</span>
+        </label>
+        <TextField
+          {...register("cidadeResidencial")}
+          value={cidadeValue}
+          variant="outlined"
+          placeholder="Nome da cidade"
+          helperText={errors.cidadeResidencial?.message}
+          error={!!errors.cidadeResidencial}
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label className={errors.estadoResidencial ? "text-red-600" : ""}>
+          Estado <span className="text-red-600">*</span>
+        </label>
+        <TextField
+          {...register("estadoResidencial")}
+          value={estadoValue}
+          variant="outlined"
+          placeholder="Nome do estado"
+          helperText={errors.estadoResidencial?.message}
+          error={!!errors.estadoResidencial}
+        />
+      </div>
+
+      <div className="my-6">
+        <span className="text-sm font-bold">
+          Campos Obrigatórios ( * )
+        </span>
       </div>
 
       <Button type="submit" variant="contained" size="large">
