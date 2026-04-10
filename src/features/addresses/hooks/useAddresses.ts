31154bde46750f4cd41835e2acfcd1fd64941ae1
Ryan Nardelli <ryannardelli12@gmail.com>
2026-04-10 15:11:38 -0300

    fix(212): Adicionado campo cidade para ser retornado de maneira temporária para evitar problema com tipagem

--- src/features/addresses/hooks/useAddresses.ts
+++ src/features/addresses/hooks/useAddresses.ts
@@ -12,6 +12,7 @@ export const useAddresses = () => {
     complemento: addr.complemento,
     principal: addr.active,
     type: "Casa", // Default for handle with the designer's front-end. This field does'nt return of API
+    cidade: "", // Value needs be return with API. While doesn't exists this field returned of API. This field was putted here to doesn't have problem in TypeScript
   }));
 
   return {
