ca428f44e60107808f26fb0abc4edb2eb7ce74e4
Ryan Nardelli <ryannardelli12@gmail.com>
2026-04-11 22:31:46 -0300

    feat: Adiconada novas alterações em conformidade com a main

--- src/types/professional.ts
+++ src/types/professional.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export type Address = {
   active: boolean;
   address: string;
@@ -12,10 +13,41 @@ export type Clinic = {
   _id: string;
   name: string;
   addition: string;
+=======
+// export type IProfessional = {
+//   id: number;
+//   name: string;
+//   specialization: string;
+//   image: string;
+//   price: number;
+//   rating: number;
+//   reviews: number;
+//   isFavorite: boolean;
+//   accessibility: string[];
+//   preferablyServices: {
+//     id: number;
+//     name: string;
+//   }[];
+//   distance?: number;
+// };
+
+export type Address = {
   cep: string;
   address: string;
   neighborhood: string;
   number: string;
+  city?: string;
+  state?: string;
+};
+
+export type Clinic = {
+  name: string;
+>>>>>>> 9f18420 (style: Adicionado opções de customizações e acrescentando imagem avatar para quando não houver imagem vindo da api)
+  cep: string;
+  address: string;
+  neighborhood: string;
+  number: string;
+<<<<<<< HEAD
   city?: string;
   state?: string;
 };
@@ -44,6 +76,32 @@ export type IProfessional = {
 
   address: Address[];
   clinic: Clinic;
+=======
+};
+
+export type IProfessional = {
+    _id: string;
+    name: string;
+    imageUrl: string;
+    CNPJCPFProfissional: string;
+    birthdayDate: string;
+    ratingsAvg: number;
+    ratingsCount: number;
+
+    professionalSpecialties: string[];
+    otherProfessionalSpecialties: string[];
+    professionalServicePreferences: string[];
+
+    acceptedPayments: {
+      pix: boolean;
+      wellhub: boolean;
+      mastercard: boolean;
+      visa: boolean;
+    };
+
+    address: Address[];
+    clinic: Clinic;
+>>>>>>> 9f18420 (style: Adicionado opções de customizações e acrescentando imagem avatar para quando não houver imagem vindo da api)
 };
 
 export type ICreateProfissional = {
