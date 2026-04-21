7961fd031f33b1d42a62c56dd102280554d58a16
Ryan Nardelli <ryannardelli12@gmail.com>
2026-04-21 13:01:29 -0300

    fix(212): Added filter to catch valid ids in hook useAddresses

--- src/features/addresses/hooks/useAddresses.ts
+++ src/features/addresses/hooks/useAddresses.ts
@@ -1,19 +1,27 @@
 import { useGetAddress } from "@/kubb";
+import { Address } from "@/types/address";
 
 export const useAddresses = () => {
   const query = useGetAddress();
+  
+  const adaptedData: Address[] =
+    query.data?.addresses
+      ?.map((addr) => {
+        if (!addr._id) return null;
 
-  const adaptedData = query.data?.addresses?.map((addr) => ({
-    id: addr._id,
-    bairro: addr.bairro,
-    rua: addr.endereco,
-    estado: addr.estado,
-    cep: addr.cep,
-    complemento: addr.complemento,
-    principal: addr.active,
-    type: addr.type,
-    cidade: addr.cidade,
-  }));
+        return {
+          id: addr._id,
+          bairro: addr.bairro ?? "",
+          rua: addr.endereco ?? "",
+          estado: addr.estado ?? "",
+          cep: addr.cep ?? "",
+          complemento: addr.complemento ?? "",
+          principal: addr.active ?? false,
+          type: addr.type ?? "",
+          cidade: addr.cidade ?? "",
+        };
+      })
+      .filter((addr): addr is Address => addr !== null) || [];
 
   return {
     ...query,
