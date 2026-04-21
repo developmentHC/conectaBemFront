e91d8f1ab39cbe07ddc56e91d288aae99b30dfa7
Ryan Nardelli <ryannardelli12@gmail.com>
2026-04-21 13:06:16 -0300

    perf: Created separation with folder adapater in features/addresses to improve performance and keep code more clean

--- src/features/addresses/hooks/useAddresses.ts
+++ src/features/addresses/hooks/useAddresses.ts
@@ -1,26 +1,13 @@
 import { useGetAddress } from "@/kubb";
 import { Address } from "@/types/address";
+import { toAddressProps } from "../adapters/toAddressProps";
 
 export const useAddresses = () => {
   const query = useGetAddress();
-  
-  const adaptedData: Address[] =
-    query.data?.addresses
-      ?.map((addr) => {
-        if (!addr._id) return null;
 
-        return {
-          id: addr._id,
-          bairro: addr.bairro ?? "",
-          rua: addr.endereco ?? "",
-          estado: addr.estado ?? "",
-          cep: addr.cep ?? "",
-          complemento: addr.complemento ?? "",
-          principal: addr.active ?? false,
-          type: addr.type ?? "",
-          cidade: addr.cidade ?? "",
-        };
-      })
+   const adaptedData: Address[] =
+    query.data?.addresses
+      ?.map(toAddressProps)
       .filter((addr): addr is Address => addr !== null) || [];
 
   return {
